d88e2e2b59735dafa01103be2d1f387bd96d8f83
vysakhjr <r.vysakh98@gmail.com>
2026-06-12 21:13:49 +0530

    Create expensetracker.py

---
+++ Python/ExpenseTracker/expensetracker.py
@@ -0,0 +1,6 @@
+def get_expense_details():
+    expense_title=input("expense_title: ")
+    expense_amount=int(input("expense_amount: "))
+    return expense_title, expense_amount
+
+value=get_expense_details()
