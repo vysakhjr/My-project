05f51d953f8bbdf7378cf39b87dc506b7b312606
Vysakh R <r.vysakh98@gmail.com>
2026-07-19 19:10:43 +0530

    Update variable.tf

--- GCP/terraform/Build Infrastructure with Terraform on Google Cloud/variable.tf
+++ GCP/terraform/Build Infrastructure with Terraform on Google Cloud/variable.tf
@@ -1,1 +1,15 @@
-
+variable "region" {
+    type = string
+    default = "us-east1"
+    description = "To define the region"
+}
+variable "zone" {
+    type = string
+    default = "us-east1-a"
+    description = "To define the zone"
+}
+variable "project_id" {
+    type = string
+    default = "xxxx"
+    description = "To define the project id"
+}
